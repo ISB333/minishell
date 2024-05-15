893710effe88b0ce074e24b35ff6ec6d03886188
Alexandre Desille <adesille@k0r3p12.42mulhouse.fr>
2024-05-15 15:00:51 +0200

    Yeah

---
+++ minishell.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adesille <adesille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/15 14:24:05 by adesille          #+#    #+#             */
+/*   Updated: 2024/05/15 14:30:23 by adesille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include <dirent.h>
+# include <string.h>
+# include <termios.h>
+# include <termcap.h>
+# include <curses.h>
+
+
+#endif
